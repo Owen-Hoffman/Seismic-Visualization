b48b8ec1c2f8f040f085561b972e3058dc7257ad
Samuel Nguyen <samuel.nguyen@wallawalla.edu>
2022-02-25 12:53:26 -0800

    test spline

---
+++ seismic_visualization/lib/graph.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'dart:async';
+import 'dart:math' as math;
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'graph home page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<LiveData> chartData;
+  late ChartSeriesController _chartSeriesController;
+
+  @override
+  void initState() {
+    chartData = getChartData();
+    Timer.periodic(const Duration(seconds: 5), updateDataSource);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Color> color = <Color>[];
+    color.add(Colors.blue[50]!);
+    color.add(Colors.blue[200]!);
+    color.add(Colors.blue);
+
+    final List<double> stops = <double>[];
+    stops.add(0.0);
+    stops.add(0.5);
+    stops.add(1.0);
+
+    final LinearGradient gradientColors =
+        LinearGradient(colors: color, stops: stops);
+    return SafeArea(
+        child: Scaffold(
+            body: SfCartesianChart(
+                series: <ChartSeries<LiveData, int>>[
+          SplineSeries<LiveData, int>(
+            onRendererCreated: (ChartSeriesController controller) {
+              _chartSeriesController = controller;
+            },
+            dataSource: chartData,
+            color: const Color.fromRGBO(192, 108, 132, 1),
+            xValueMapper: (LiveData sales, _) => sales.time,
+            yValueMapper: (LiveData sales, _) => sales.speed,
+            gradient: gradientColors,
+          )
+        ],
+                primaryXAxis: CategoryAxis(
+                    majorGridLines: const MajorGridLines(width: 0),
+                    edgeLabelPlacement: EdgeLabelPlacement.shift,
+                    interval: 3,
+                    title: AxisTitle(text: 'Time (minutes)')),
+                primaryYAxis: NumericAxis(
+                    axisLine: const AxisLine(width: 0),
+                    majorTickLines: const MajorTickLines(size: 0),
+                    title: AxisTitle(text: 'Magnitude')))));
+  }
+
+  int time = 19;
+  void updateDataSource(Timer timer) {
+    chartData.add(LiveData(time++, (math.Random().nextInt(60) + 30)));
+    chartData.removeAt(0);
+    _chartSeriesController.updateDataSource(
+        addedDataIndex: chartData.length - 1, removedDataIndex: 0);
+  }
+
+  List<LiveData> getChartData() {
+    return <LiveData>[
+      LiveData(0, 42),
+      LiveData(1, 47),
+      LiveData(2, 43),
+      LiveData(3, 49),
+      LiveData(4, 54),
+      LiveData(5, 41),
+      LiveData(6, 58),
+      LiveData(7, 51),
+      LiveData(8, 98),
+      LiveData(9, 41),
+      LiveData(10, 53),
+      LiveData(11, 72),
+      LiveData(12, 86),
+      LiveData(13, 52),
+      LiveData(14, 94),
+      LiveData(15, 92),
+      LiveData(16, 86),
+      LiveData(17, 72),
+      LiveData(18, 94)
+    ];
+  }
+}
+
+class LiveData {
+  LiveData(this.time, this.speed);
+  final int time;
+  final num speed;
+}
