5a3eb90ec805b454e2e3ec4b9f117ac206794279
Samuel Nguyen <samuel.nguyen@wallawalla.edu>
2022-02-25 12:26:38 -0800

    data

---
+++ seismic_visualization/lib/data.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void getData() async {
+    Response response =
+        await get('https://jsonplaceholder.typicode.com/todos/1');
+    // print(response.body);
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('loading screen'),
+    );
+  }
+}
