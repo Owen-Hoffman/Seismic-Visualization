439da4136ea663e70693e3081c5510d288f09b7a
Samuel Nguyen <samuel.nguyen@wallawalla.edu>
2022-03-04 13:35:30 -0800

    test

--- seismic_visualization/lib/main.dart
+++ seismic_visualization/lib/main.dart
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          js.context.callMethod('doPlot', ['Flutter is calling upon JavaScript!']);
+          js.context.callMethod('doPlot',[State()]);
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
